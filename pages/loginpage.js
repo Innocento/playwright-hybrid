a24ac243d2bfd2cd9776203bc2f5fde1f60dacdf
Innocento <omidoinnocent32@gmail.com>
2025-07-16 21:44:34 +0300

    some more changes version 2

--- pages/loginpage.js
+++ pages/loginpage.js
@@ -1,13 +1,22 @@
 const {test, expect} = require('@playwright/test')
 
-test('My test1',async function(page) {
-    expect(12).toBe(12)
-})
+exports.LoginPage = class LoginPage {
+    constructor(page) {
+        this.page = page;
+        this.emailField = page.locater('id=input-email')
+        this.passwordField = page.locater('id=input-password')
+        this.loginButton = page.locater('input:has-text("Login")')
+    }
 
-test1('My test3',async function(page) {
-    expect(13).toBe(15)
-})
+    async enterEmail(email) {
+        await this.emailField.fill(email)
+    }
 
-test2('My test2',async function(page) {
-    expect(15).toBe(15)
-})
+    async enterPassword(password) {
+        await this.passwordField(password)
+    }
+
+    async clickLogin() {
+        await this.loginButton()
+    }
+}
